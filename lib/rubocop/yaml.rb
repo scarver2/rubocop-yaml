037093d8a2619a5eb887e012bf93b00ba898a7e5
Stan Carver II <stan@a1webconsulting.com>
2026-09-06 15:32:54 -0500

    feat: add invalid YAML syntax cop

--- lib/rubocop/yaml.rb
+++ lib/rubocop/yaml.rb
@@ -5,3 +5,4 @@ require_relative "yaml/parser"
 # Plugin registration reads VERSION while the class is defined.
 require_relative "yaml/version"
 require_relative "yaml/plugin"
+require_relative "cop/yaml/lint/invalid_syntax"
