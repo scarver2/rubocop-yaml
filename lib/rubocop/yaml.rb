241d3a282c8a37f8deca47ef112c2c711f9962f4
Stan Carver II <stan@a1webconsulting.com>
2026-09-15 06:11:31 -0500

    refactor: use ruby-i18n interpolation semantics

--- lib/rubocop/yaml.rb
+++ lib/rubocop/yaml.rb
@@ -1,11 +1,14 @@
 # lib/rubocop/yaml.rb
 # frozen_string_literal: true
 
+require "i18n"
+
 require_relative "yaml/parser"
 require_relative "yaml/processed_source"
 # Plugin registration reads VERSION while the class is defined.
 require_relative "yaml/version"
 require_relative "yaml/plugin"
+require_relative "yaml/rails/i18n/interpolation"
 require_relative "yaml/rails/i18n/locale_index"
 require_relative "yaml/rails/i18n/locale_index_builder"
 require_relative "yaml/schema/registry"
